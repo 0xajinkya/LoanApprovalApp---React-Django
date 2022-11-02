178779d789dd31238495e31271bfdd21bacb6291
ajinkyaKahane <kahaneajinkya051@gmail.com>
2022-11-03 01:11:39 +0530

    Backend Started

--- ModelBackend/firstPage/views.py
+++ ModelBackend/firstPage/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+import joblib
+
+model = joblib.load('modelPipeline.pkl')
+
+def scoreJson(request):
+    
+    return JsonResponse({'score': 1})
+
+def scoreFile(request):
+    return JsonResponse({'score': 1})
+
